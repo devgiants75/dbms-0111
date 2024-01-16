cc0a7235451ebfbf49d3a8ab75a1be8854c8cd4f
devgiants75 <devgiants75@naver.com>
2024-01-16 20:42:25 +0900

    20240116

---
+++ 0116/c_SQL명령어종류.sql
@@ -0,0 +1,31 @@
+# SQL(Structured Query Language)
+# : 데이터베이스 관리 시스템(DBMS)에서 데이터를 관리하기 위해
+# : , 사용되는 표준 프로그래밍 언어 
+# - 데이터베이스에 저장된 데이터를 조회, 삽입, 수정, 삭제하는데 사용
+# - 데이터베이스의 구조를 정의하고 조작하는데 사용
+
+# SQL 주요 구성 요소
+# : DBMS에서 실행 할 수 있는 명령의 종류에 따라
+# : DDL, DML, DCL로 분류
+
+# 1. DDL(Data Definition Language)
+# : 데이터 정의 언어
+# : 데이터베이스 스키마와 구조를 정의하는데 사용
+# : create, drop, alter, truncate 
+
+# 2. DML(Data Manipulation Language)
+# : 데이터 조작 언어
+# : 테이블에 데이터 검색, 등록, 수정, 삭제하는데 사용
+# : select, insert, update, delete
+
+# 3. DCL(Data Control Language)
+# : 데이터 제어 언어
+# : 데이터베이스에서 사용자 권한을 관리하는 데 사용
+# : grant, revoke
+ 
+# SQL의 특징
+# - 비절차적 언어
+# : SQL은 사용자가 원하는 것을 선언하는 언어
+# : '무엇을' 얻고 싶은지를 명시 (어떻게 얻을 것인지 X)
+# - 표준화
+# : ANSI에 의해 표준화, 대부분 관계형데이터베이스 시스템에 지원(테이블)
